008102e1c54445bb57df643068a059b1018c4cf3
artumarinn <artumarin.09@gmail.com>
2025-05-28 00:54:47 -0300

    docker built

--- backend/app/services/web3_client.py
+++ backend/app/services/web3_client.py
@@ -10,20 +10,25 @@ web3 = Web3(Web3.HTTPProvider(node_url))
 if web3.is_connected():
     print("🧩Conectado al nodo UCH")
     
-    # Obtener el número del ultimo bloque
+    # # Obtener el número del ultimo bloque
     latest_block = web3.eth.block_number
     print(f"🟩Ultimo bloque: {latest_block}")
     
-    # Obtener información del ultimo bloque
+    # # Obtener información del ultimo bloque
     block = web3.eth.get_block(latest_block)
     print(f"🟩Informacion del bloque:\n{block}")
     
-    # Obtener el numero de peers conectados
+    # # Obtener el numero de peers conectados
     peer_count = web3.net.peer_count
     print(f"🟩Peers conectados: {peer_count}")
     
-    # Obtener la version del cliente
+    # # Obtener la version del cliente
     client_version = web3.client_version
     print(f"🟩Version del cliente: {client_version}")
+
+    # Obtener el balance de una cuenta
+    balance = web3.eth.get_balance("0xe5061FE5b4d0BD260F1Ff80FA919e339F1f5C330")
+    print(f"🟩Balance de la cuenta: {web3.from_wei(balance, 'ether')}")
+
 else:
     print("🟥No se pudo conectar al nodo UCH")
